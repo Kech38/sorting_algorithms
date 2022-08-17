3f363bcb2b25c149739e883f48753644629eb122
Kech38 <nasuai3880@gmail.com>
2022-08-16 17:53:55 -0700

    made some corrections

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,48 +1,31 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
+ * bubble_sort - sorts an array in ascending order
+ * @array: array of ints to sort
+ * @size: size of the array
  */
-void swap_ints(int *a, int *b)
-{
-	int tmp;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-
-/**
- * bubble_sort - sorts bubbly
- * @array: array to sort
- * @size: size
- *
- */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, newsize = size;
-	int swap;
+unsigned int i, j;
+int tmp;
 
-	if (array == NULL || size < 2)
-		return;
+if (size < 2)
+return;
 
-	while (newsize)
-	{
-		swap = 0;
-		for (i = 0; i < (size - 1); i++)
-		{
-			if (array[i] > array[i + 1])
-			{
-				swap_ints(&array[i], &array[i + 1]);
-				print_array(array, size);
-				swap = 1;
-			}
-		}
-		newsize--;
-		if (swap == 0)
-			break;
-	}
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+tmp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = tmp;
+print_array(array, size);
+}
+}
+}
 }
